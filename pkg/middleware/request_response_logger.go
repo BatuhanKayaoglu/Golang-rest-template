091be5b31e04340f23381dbd1cd85fd9f815b215
batuhankayaoglu <batuhan.kayaoglu@apptotech.com>
2026-01-10 17:52:59 +0300

    fix readme.

--- pkg/middleware/request_response_logger.go
+++ pkg/middleware/request_response_logger.go
@@ -19,7 +19,6 @@ import (
 	"go.uber.org/zap"
 )
 
-// LogRequestModel represents the request details in the log
 type LogRequestModel struct {
 	T                             string            `bson:"_t" json:"_t"`
 	RequestDateTime               time.Time         `bson:"RequestDateTime" json:"RequestDateTime"`
@@ -33,7 +32,6 @@ type LogRequestModel struct {
 	RequestQueryParams            map[string]string `bson:"RequestQueryParams" json:"RequestQueryParams"`
 }
 
-// LogResponseModel represents the response details in the log
 type LogResponseModel struct {
 	T                              string            `bson:"_t" json:"_t"`
 	ResponseDateTime               time.Time         `bson:"ResponseDateTime" json:"ResponseDateTime"`
@@ -45,7 +43,6 @@ type LogResponseModel struct {
 	ResponseContentType            string            `bson:"ResponseContentType" json:"ResponseContentType"`
 }
 
-// RequestResponseLogModel represents the complete log entry format
 type RequestResponseLogModel struct {
 	T                           string            `bson:"_t" json:"_t"`
 	Type                        string            `bson:"Type" json:"Type"`
@@ -66,7 +63,6 @@ type RequestResponseLogModel struct {
 	IsExceptionActionLevel      *bool             `bson:"IsExceptionActionLevel" json:"IsExceptionActionLevel"`
 }
 
-// responseBodyWriter wraps gin.ResponseWriter to capture response body
 type responseBodyWriter struct {
 	gin.ResponseWriter
 	body *bytes.Buffer
@@ -77,7 +73,6 @@ func (w responseBodyWriter) Write(b []byte) (int, error) {
 	return w.ResponseWriter.Write(b)
 }
 
-// extractUserFromToken extracts user information from JWT token
 func extractUserFromToken(c *gin.Context) (userId, username string) {
 	const BearerSchema = "Bearer "
 	header := c.GetHeader("Authorization")
@@ -96,17 +91,13 @@ func extractUserFromToken(c *gin.Context) (userId, username string) {
 		return "", ""
 	}
 
-	// Issuer contains the username based on GenerateToken implementation
 	return claims.UserId, claims.Username
 }
 
-// getLocalIP returns the local IP address
 func getLocalIP() string {
-	// In production, you might want to get the actual server IP
 	return "::ffff:127.0.0.1"
 }
 
-// getEnvironment returns the environment name (hostname/pod name)
 func getEnvironment() string {
 	hostname, err := os.Hostname()
 	if err != nil {
@@ -115,40 +106,29 @@ func getEnvironment() string {
 	return hostname
 }
 
-// getOsVersion returns the OS version
 func getOsVersion() string {
 	return runtime.GOOS
 }
-
-// RequestResponseLogger middleware logs request and response details to MongoDB
 func RequestResponseLogger(logger *zap.Logger, collection *mongo.Collection) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		// Start timer
 		startTime := time.Now().UTC()
-
-		// Generate unique LogId and TraceId
 		logId := uuid.New().String()
 		traceId := c.GetHeader("X-Trace-Id")
 		if traceId == "" {
 			traceId = uuid.New().String()
 		}
 
-		// Extract user info from JWT token
 		userId, username := extractUserFromToken(c)
 
-		// Store in context for potential use in handlers
 		c.Set("logId", logId)
 		c.Set("traceId", traceId)
 
-		// Capture request body
 		var requestBodyBytes []byte
 		var requestBodyParsed interface{}
 		if c.Request.Body != nil {
 			requestBodyBytes, _ = io.ReadAll(c.Request.Body)
-			// Restore the request body for handlers to read
 			c.Request.Body = io.NopCloser(bytes.NewBuffer(requestBodyBytes))
 
-			// Parse request body as JSON if possible
 			if len(requestBodyBytes) > 0 {
 				if err := json.Unmarshal(requestBodyBytes, &requestBodyParsed); err != nil {
 					requestBodyParsed = string(requestBodyBytes)
@@ -156,11 +136,9 @@ func RequestResponseLogger(logger *zap.Logger, collection *mongo.Collection) gin
 			}
 		}
 
-		// Get request headers
 		requestHeaders := make(map[string]string)
 		for key, values := range c.Request.Header {
 			if len(values) > 0 {
-				// Mask sensitive headers
 				if strings.ToLower(key) == "authorization" {
 					requestHeaders[key] = "***"
 				} else {
@@ -169,7 +147,6 @@ func RequestResponseLogger(logger *zap.Logger, collection *mongo.Collection) gin
 			}
 		}
 
-		// Get query parameters
 		queryParams := make(map[string]string)
 		for key, values := range c.Request.URL.Query() {
 			if len(values) > 0 {
@@ -177,7 +154,6 @@ func RequestResponseLogger(logger *zap.Logger, collection *mongo.Collection) gin
 			}
 		}
 
-		// Log REQUEST to MongoDB
 		requestLogEntry := RequestResponseLogModel{
 			T:                           "RequestResponseLogModel",
 			Type:                        "Api",
@@ -208,12 +184,9 @@ func RequestResponseLogger(logger *zap.Logger, collection *mongo.Collection) gin
 			IsExceptionActionLevel: nil,
 		}
 
-		// Log request to MongoDB
 		if _, err := collection.InsertOne(context.TODO(), requestLogEntry); err != nil {
 			logger.Error("Failed to log request to MongoDB", zap.Error(err))
 		}
-
-		// Wrap the response writer to capture response body
 		responseBody := &bytes.Buffer{}
 		writer := responseBodyWriter{
 			ResponseWriter: c.Writer,
@@ -221,14 +194,11 @@ func RequestResponseLogger(logger *zap.Logger, collection *mongo.Collection) gin
 		}
 		c.Writer = writer
 
-		// Process request
 		c.Next()
 
-		// End timer
 		endTime := time.Now().UTC()
 		responseTime := time.Now().UTC()
 
-		// Get response headers
 		responseHeaders := make(map[string]string)
 		for key, values := range c.Writer.Header() {
 			if len(values) > 0 {
@@ -236,13 +206,11 @@ func RequestResponseLogger(logger *zap.Logger, collection *mongo.Collection) gin
 			}
 		}
 
-		// Parse response body as JSON if possible
 		var responseBodyParsed interface{}
 		if err := json.Unmarshal(responseBody.Bytes(), &responseBodyParsed); err != nil {
 			responseBodyParsed = responseBody.String()
 		}
 
-		// Log RESPONSE to MongoDB
 		responseLogEntry := RequestResponseLogModel{
 			T:                           "RequestResponseLogModel",
 			Type:                        "Api",
@@ -271,7 +239,6 @@ func RequestResponseLogger(logger *zap.Logger, collection *mongo.Collection) gin
 			IsExceptionActionLevel: nil,
 		}
 
-		// Log response to MongoDB
 		if _, err := collection.InsertOne(context.TODO(), responseLogEntry); err != nil {
 			logger.Error("Failed to log response to MongoDB", zap.Error(err))
 		}
